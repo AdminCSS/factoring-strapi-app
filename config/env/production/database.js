89cdc4fc794aaf3fb21ba4075dcdfd42375c8011
Josep Romagosa <josepromll@gmail.com>
2023-03-28 17:10:10 -0600

    Fixes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,39 +3,19 @@ const parse = require("pg-connection-string").parse;
 const { host, port, database, user, password } = parse(
   process.env.DATABASE_URL
 );
-
-module.exports = ({ env }) => ({
+module.exports = () => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
       host,
       port,
       database,
       user,
       password,
-      ssl: env.bool('DATABASE_SSL', false) && {
-        key: env('DATABASE_SSL_KEY', undefined),
-        cert: env('DATABASE_SSL_CERT', undefined),
-        ca: env('DATABASE_SSL_CA', undefined),
-        capath: env('DATABASE_SSL_CAPATH', undefined),
-        cipher: env('DATABASE_SSL_CIPHER', undefined),
-        rejectUnauthorized: env.bool(
-          'DATABASE_SSL_REJECT_UNAUTHORIZED',
-          true
-        ),
+      ssl: {
+        rejectUnauthorized: false,
       },
     },
     debug: false,
-    acquireConnectionTimeout: 1000000,
-    pool: {
-      min: 0,
-      max: 1,
-      acquireTimeoutMillis: 600000,
-      createTimeoutMillis: 600000,
-      destroyTimeoutMillis: 600000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis:1000,
-      createRetryIntervalMillis: 2000
-    },
   },
 });
