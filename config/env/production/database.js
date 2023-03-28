c88490635a681b5aa1989deaf7d6ef0ea41781fb
Josep Romagosa <josepromll@gmail.com>
2023-03-28 17:07:14 -0600

    Fixes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -13,7 +13,17 @@ module.exports = ({ env }) => ({
       database,
       user,
       password,
-      ssl: true
+      ssl: env.bool('DATABASE_SSL', false) && {
+        key: env('DATABASE_SSL_KEY', undefined),
+        cert: env('DATABASE_SSL_CERT', undefined),
+        ca: env('DATABASE_SSL_CA', undefined),
+        capath: env('DATABASE_SSL_CAPATH', undefined),
+        cipher: env('DATABASE_SSL_CIPHER', undefined),
+        rejectUnauthorized: env.bool(
+          'DATABASE_SSL_REJECT_UNAUTHORIZED',
+          true
+        ),
+      },
     },
     debug: false,
     acquireConnectionTimeout: 1000000,
